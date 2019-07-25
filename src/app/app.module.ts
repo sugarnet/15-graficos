4b1b3e45a4de1f1e18054b7198b43f4f70610c0a
Diego Scifo <diegoscifo@gmail.com>
2019-07-25 13:26:22 -0300

    desde casa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { LineComponent } from './components/line/line.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
